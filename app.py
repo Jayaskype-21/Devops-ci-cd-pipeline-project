d21520fdffc65ade8b70bf7f570469df97bb323b
Jayasree K <146369872+Jayaskype-21@users.noreply.github.com>
2026-02-18 21:30:40 +0530

    Fix Milk price in grocery_items list
    Updated the price of Milk from 200 to 100.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 grocery_items = [
     {"id": 1, "name": "Rice", "price": 50},
-    {"id": 2, "name": "Milk", "price": 200},
+    {"id": 2, "name": "Milk", "price": 100},
     {"id": 3, "name": "Sugar", "price": 300}
 ]
 
